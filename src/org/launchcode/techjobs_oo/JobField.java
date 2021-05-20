eb67494cf4058b45c3230aa3273ba162053786b6
Jarod Waggener <jarod2228@rocketmail.com>
2021-05-20 18:36:43 -0500

    apparently added something

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -4,16 +4,21 @@ public class JobField {
 /*
 A)
 Things the four support classes have in common:
-{
 private int id;
 private static int nextId = 1;
 private String value;
-}
 B)
+Constructors that are the same:
 the id assignment constructor is identical
 the value assignment constructor is identical
-
 C)
+getters and setters that are the same:
+getId()
+getValue()
+setValue()
 D)
+custom methods that are the same:
+toString()
+
 */
 }
