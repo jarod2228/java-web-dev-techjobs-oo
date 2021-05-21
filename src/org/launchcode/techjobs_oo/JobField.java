10cd9af3aa724fb699b4160652cd1ab932231536
Jarod Waggener <jarod2228@rocketmail.com>
2021-05-20 20:08:58 -0500

    added abstract class JobField.java and extended other classes to DRY the code, removing redundant code from JobField's new child classes

--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -1,6 +1,53 @@
 package org.launchcode.techjobs_oo;
 
-public class JobField {
+import java.util.Objects;
+
+public abstract class JobField {
+
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField)) return false;
+        JobField that = (JobField) o;
+        return this.getId() == that.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
 /*
 A)
 Things the four support classes have in common:
@@ -19,6 +66,6 @@ setValue()
 D)
 custom methods that are the same:
 toString()
-
+equals()
+hashCode()
 */
-}
